b23f346dc3fe02200042c8eeec35cc33cd486fad
Aleksey Pashkov <pashkov.a.n@yandex.ru>
2025-01-09 19:09:50 +0500

    nested groups

--- src/DirectorySync.Infrastructure/Integrations/Ldap/LdapFilters.cs
+++ src/DirectorySync.Infrastructure/Integrations/Ldap/LdapFilters.cs
@@ -12,6 +12,7 @@ internal static partial class LdapFilters
     public static string FindEnabledGroupMembersByGroupDn(string groupDn)
     {
         // NOT disabled: UAC flags does not contain UF_ACCOUNT_DISABLE
-        return $"(&(objectClass=user)(memberof={groupDn})(!userAccountControl:1.2.840.113556.1.4.803:=2))";
+        // Active Directory only.
+        return $"(&(objectClass=user)(memberof:1.2.840.113556.1.4.1941:={groupDn})(!userAccountControl:1.2.840.113556.1.4.803:=2))";
     }
 }
